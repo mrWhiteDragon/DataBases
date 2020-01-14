b595a42ff67686a589fe569633140459456038fd
Sergey Solodyannikov <mr.whitedragon@mail.ru>
2020-01-14 19:05:58 +0500

    Added folder with completed tasks for lesson 8

---
+++ lesson_8/db_8_3_4.sql
@@ -0,0 +1,22 @@
+-- 3)Переписать запросы, заданые к ДЗ урока 6 с использованием JOIN
+--  (четыре запроса).
+
+-- 4.Найти 10 пользователей, которые проявляют наименьшую активность в
+-- использовании социальной сети.
+
+SELECT users.id,
+  COUNT(DISTINCT messages.id) + 
+  COUNT(DISTINCT likes.id) + 
+  COUNT(DISTINCT media.id) AS activity 
+  FROM users
+    LEFT JOIN messages 
+      ON users.id = messages.from_user_id
+    LEFT JOIN likes
+      ON users.id = likes.user_id
+    LEFT JOIN media
+      ON users.id = media.user_id
+  GROUP BY users.id
+  ORDER BY activity
+  LIMIT 10;
+
+
